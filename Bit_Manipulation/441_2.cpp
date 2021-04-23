bc77ca398edabda1f206fcff58b502d032a0c45d
Avantika Singh <77248593+Avantika782@users.noreply.github.com>
2021-04-23 23:22:26 +0530

    Recursive method for 441

---
+++ Bit_Manipulation/441_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int setBit(int n){
+    if(n==0){
+        return 0;
+    }
+    else
+    return (n&1)+setBit(n >> 1);
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout << setBit(n) << endl;
+            }
+    return 0;
+}
+
+//Recusive Method
