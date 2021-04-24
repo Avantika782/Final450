186d1a0c4519d87bdc20b97a584caf7ffcd9579a
Avantika Singh <77248593+Avantika782@users.noreply.github.com>
2021-04-24 23:24:36 +0530

    Reverse an Array

---
+++ Array/450_1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void reverseArray(int arr[], int size)
+{
+    int h = size/2;
+    for(int i =0; i < h; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
+    }
+}
+
+void reverse(int arr[], int start, int end)
+{
+    while(start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements in array: ";
+    cin >> n;
+    int arr[n];
+    cout << endl <<"Enter the elemnents: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    reverseArray(arr, n);
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << "   ";
+    return 0;
+}
